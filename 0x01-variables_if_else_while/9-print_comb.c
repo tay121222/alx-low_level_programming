86d1edecf182a8e8bb64cf87c35b4820641818ec
Enoch Taylor <tay121222@gmail.com>
2023-03-20 23:44:46 +0000

    program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		int n = 0;
+
+while (n < 10)
+{
+		putchar(48 + n);
+		if (n == 9)
+		{
+			putchar('\n');
+			n++;
+		}
+		else
+		{
+		putchar(',');
+		putchar(' ');
+		n++;
+		}
+}
+
+return (0);
+}
