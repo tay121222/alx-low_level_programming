ed943bc1c02b058352fedbe40cb9d8d8eb502c66
Enoch Taylor <tay121222@gmail.com>
2023-03-20 15:24:25 +0000

    rogram that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Program Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(c);
+		alphabet++;
+	}
+}
