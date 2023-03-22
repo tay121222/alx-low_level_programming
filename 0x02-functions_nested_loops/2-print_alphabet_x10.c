7c1582550fe1454263b3fdde5ac6758e86d62d51
Enoch Taylor <tay121222@gmail.com>
2023-03-22 09:53:20 +0000

    Updating

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Generate the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int n = 0;
+	while (n < 10)
+	{
+		char alphabet = 'a';
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
