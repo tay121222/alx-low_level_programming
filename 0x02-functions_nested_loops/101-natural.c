ca064158e40f2074e92e9d14f113ec809a3e5efd
Enoch Taylor <tay121222@gmail.com>
2023-03-23 14:01:20 +0000

    list all the natural numbers below 10 that are multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - program start
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sum = 0, int i = 1;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5) == 0)
+		{
+			sum += i;
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
