76e724c56ce8397e4d7b7faa5434375bbfcea9da
Enoch Taylor <tay121222@gmail.com>
2023-03-25 04:15:02 +0000

    Fizz Buzz Task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ *
+ * Return: always return 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((n % 5) == 0 && (n % 3) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
