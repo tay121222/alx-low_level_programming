261a9834ca64a9859296465c332e9c0021f47954
Enoch Taylor <tay121222@gmail.com>
2023-03-25 05:00:50 +0000

    Fizz Buzz Task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,17 @@ int main(void)
 
 	while (n <= 100)
 	{
-		if ((n % 3) == 0)
+		if ((n % 5 == 0) && (n % 3 == 0))
+   		{
+			printf("FizzBuzz ");
+		}
+		else if ((n % 3) == 0)
 		{
 			printf("Fizz ");
 		}
 		else if ((n % 5) == 0)
 		{
 			printf("Buzz ");
-		}
-		else if ((n % 5) == 0 && (n % 3) == 0)
-		{
-			printf("FizzBuzz ");
 		}
 		else
 		{
