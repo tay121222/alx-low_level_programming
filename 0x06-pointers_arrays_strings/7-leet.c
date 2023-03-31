0f669fc49fed8687984ceae65348d016600c3e1d
Enoch Taylor <tay121222@gmail.com>
2023-03-31 18:37:15 +0100

    function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * leet - Function that encodes a string into 1337
+ * @s: string to encode
+ * Return: returns encoded string
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "443307711";
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (letters[j])
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (s);
+}
