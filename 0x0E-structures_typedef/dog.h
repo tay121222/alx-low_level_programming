5df99ca530b887cef33376af8122fcc5781a7338
Enoch Taylor <tay121222@gmail.com>
2023-04-19 04:18:21 +0000

    Adding type struct dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOGGIE
+#define DOGGIE
+/**
+ * struct dog - Dog data
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner:Dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} doggie;
+#endif
