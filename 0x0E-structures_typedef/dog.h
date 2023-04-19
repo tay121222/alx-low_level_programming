36335424a512e7205dd8992091334840496231ac
Enoch Taylor <tay121222@gmail.com>
2023-04-19 05:02:15 +0000

    updaing do header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,7 +11,10 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} doggie;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
