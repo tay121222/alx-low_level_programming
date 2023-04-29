4f05e5d178ef21b94bd7c58114e35347f01ee056
Enoch Taylor <tay121222@gmail.com>
2023-04-29 08:16:43 +0000

    64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,15 +2,13 @@ extern printf
 
 section .data
 msg: db "Hello, Holberton", 10
-fmt: db "%s", 10, 0
 
 section .text
 global main
 
 main:
 push rbp
-mov rdi, fmt
-mov rsi, msg
+mov rdi, msg
 mov rax, 0
 call printf
 pop rbp
