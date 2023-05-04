ef8817fd9e2fc30217b4dad9d7700c4d80d802b2
Enoch Taylor <tay121222@gmail.com>
2023-05-04 08:10:52 +0000

    Modified 101-print_listint_safe file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,30 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * print_listint_safe -  function that prints a linked list
+ * free_listp - frees a linked list
+ * @head: head of a list
+ * Return: no return
+ */
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *c_node;
+
+	if (head != NULL)
+	{
+		c_node = *head;
+		while ((temp = c_node) != NULL)
+		{
+			c_node = c_node->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+/**
+ * print_listint_safe - function that prints a linked list
  * @head: a pointer to the head
  *
  * Return: The number of nodes in the list
@@ -9,31 +32,39 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *c_node, *loop_node;
+	const listint_t *c_node = head;
+	listp_t *hptr = NULL, *new, *add;
 
-	if (head == NULL)
-		exit(98);
-
-	c_node = head;
-	loop_node = head->next;
-
-	while (loop_node != NULL && loop_node < c_node)
+	while (c_node != NULL)
 	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+		{
+			exit(98);
+		}
+		new->p = (void *)c_node;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (c_node == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)c_node, c_node->n);
+				free_listp(&hptr);
+				return (count);
+			}
+		}
+
 		printf("[%p] %d\n", (void *)c_node, c_node->n);
-		count++;
 		c_node = c_node->next;
-		loop_node = loop_node->next;
-
+		count++;
 	}
 
-	printf("[%p] %d\n", (void *)c_node, c_node->n);
-	count++;
-
-	if (loop_node != NULL)
-	{
-		printf("-> [%p] %d\n", (void *)loop_node, loop_node->n);
-		exit(98);
-	}
+	free_listp(&hptr);
 
 	return (count);
 }
