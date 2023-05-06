a0bcc5d554146bbd97734cb5ae1b3757220114e1
Enoch Taylor <tay121222@gmail.com>
2023-05-06 23:00:31 +0000

    function that checks the endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,11 +3,12 @@
 /**
  * get_endianness - function that checks the endianness
  *
- * Returns: 0 if big endian, 1 if little endian
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
 	byte b;
+
 	b.a = 1;
 	return ((int) b.a);
 }
