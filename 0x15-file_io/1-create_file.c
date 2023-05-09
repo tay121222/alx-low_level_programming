14ffe418e2b7730dc43c8b09a03f2bff99aeb888
Enoch Taylor <tay121222@gmail.com>
2023-05-09 11:53:54 +0000

    Under the snow - Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,6 @@
  * create_file - function that creates a file
  * @filename: name of the file to create
  * @text_content: NULL terminated string to write to the file
- *
  * Return: 1 on success, -1 on failure;
  */
 int create_file(const char *filename, char *text_content)
