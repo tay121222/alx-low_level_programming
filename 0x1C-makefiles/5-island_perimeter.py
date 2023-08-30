068df16c28d7c214654fceb542c579a54d5c47c8
Enoch Taylor <tay121222@gmail.com>
2023-08-30 14:47:29 +0000

    returns the perimeter of the island described in grid updated

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,13 +8,13 @@ perimeter of the island described in grid
 def island_perimeter(grid):
     """Function returns the perimeter of the island described in grid"""
     m, n = len(grid), len(grid[0])
-    land, water = 0, 0
+    land, edge = 0, 0
     for i in range(m):
-        for j in range(m):
+        for j in range(n):
             if grid[i][j] == 1:
                 land += 1
                 if i < m - 1 and grid[i+1][j] == 1:
-                    water += 1
+                    edge += 1
                 if j < n - 1 and grid[i][j+1] == 1:
-                    water += 1
-    return (land * 4) - (2 * water)
+                    edge += 1
+    return (land * 4) - (2 * edge)
